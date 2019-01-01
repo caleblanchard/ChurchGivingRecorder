df107399f86ac3d4594e54c3d845ebe55cc56c46
Caleb Blanchard <caleblanchard@gmail.com>
2019-01-01 10:31:33 -0500

    Add project files.

---
+++ ChurchGivingRecorder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChurchGivingRecorder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
