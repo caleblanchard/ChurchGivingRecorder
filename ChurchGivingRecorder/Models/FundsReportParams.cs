a129f073035f965d4ddcaac8ae9ae7091b71042c
Caleb Blanchard <caleblanchard@gmail.com>
2019-01-08 22:24:21 -0500

    Add funds report

---
+++ ChurchGivingRecorder/Models/FundsReportParams.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChurchGivingRecorder.Models
+{
+    public enum GroupBy
+    {
+        Year,
+        Month,
+        Day
+    }
+
+    public class FundsReportParams
+    {
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        public GroupBy GroupBy { get; set; }
+    }
+}
