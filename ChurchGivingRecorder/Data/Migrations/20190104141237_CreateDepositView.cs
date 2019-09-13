b898727b3a8e789c4665ce21620ad65773f7954b
Caleb Blanchard <caleblanchard@gmail.com>
2019-09-13 16:03:14 -0400

    Make some fields required

--- ChurchGivingRecorder/Data/Migrations/20190104141237_CreateDepositView.cs
+++ ChurchGivingRecorder/Data/Migrations/20190104141237_CreateDepositView.cs
@@ -16,7 +16,7 @@ GROUP BY A.Id, A.DepositDate, A.Description");
 #else
             migrationBuilder.Sql(@"CREATE VIEW [dbo].[DepositView]
 AS
-SELECT A.Id, A.DepositDate, A.Description, Sum(C.Amount) As TotalAmount FROM dbo.Deposits A
+SELECT A.Id, A.DepositDate, A.Description, ISNULL(Sum(C.Amount),0) As TotalAmount FROM dbo.Deposits A
 LEFT JOIN dbo.Gifts B ON A.Id = B.DepositId
 LEFT JOIN dbo.GiftDetails C ON B.Id = C.GiftId
 GROUP BY A.Id, A.DepositDate, A.Description");
